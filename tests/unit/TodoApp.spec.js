d8191b56642bd087443086d85b8bb49a910ba25e
jobserver <joabe.santos@gmail.com>
2022-03-13 00:14:04 -0300

    Create todoApp

---
+++ tests/unit/TodoApp.spec.js
@@ -0,0 +1,38 @@
+import { mount } from '@vue/test-utils'
+import TodoApp from '@/components/TodoApp.vue'
+
+describe("TodoApp",()=>{
+
+    it('renders a todo', () => {
+        const wrapper = mount(TodoApp)
+        
+        const todo = wrapper.get('[data-test="todo"]')
+        
+        expect(todo.text()).toBe('Learn Vue.js 3')
+    })
+
+    test("creates a todo", async ()=>{
+        const wrapper = mount(TodoApp);
+
+        expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(1);
+
+
+       await wrapper.get('[data-test="new-todo"]').setValue('New todo')
+       await wrapper.get('[data-test="form"]').trigger('submit')
+
+        expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2)
+
+    })
+
+    test("completes a todo", async ()=>{
+        const wrapper = mount(TodoApp);
+
+       await wrapper.get('[data-test="todo-checkbox"]').setChecked(true)
+       
+       expect(wrapper.get('[data-test="todo"]').classes()).toContain('completed')
+
+    })
+
+
+
+})
